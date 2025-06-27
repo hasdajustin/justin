289b3b17bbfefd3f86d575ab0702b261aa7413ec
Matheus Justin Hasda <matheusjustinhasda@gmail.com>
2025-06-28 01:37:23 +0600

    portfolio data rendering test from database

--- core/views.py
+++ core/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
 def home(request):
     return render(request, 'core/index.html')
 
 def portfolio(request):
-    return render(request, 'core/portfolio.html')
+    projects = Project.objects.all().order_by('-created_at')
+    return render(request, 'core/portfolio.html', {'projects': projects})
 
 def contact(request):
     return render(request, 'core/contact.html')
