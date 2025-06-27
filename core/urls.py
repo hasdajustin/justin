20b950da5471312e596ea98add3fdf2eea3ee4da
Matheus Justin Hasda <matheusjustinhasda@gmail.com>
2025-06-28 01:15:41 +0600

    fix contact page

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('portfolio/', views.portfolio, name='portfolio'),
+    path('contact/', views.contact, name='contact'),
 ]
--- core/views.py
+++ core/views.py
@@ -6,3 +6,6 @@ def home(request):
 
 def portfolio(request):
     return render(request, 'core/portfolio.html')
+
+def contact(request):
+    return render(request, 'core/contact.html')
