7bdc66ec5604be15ce8475bf4d62879c1c326a62
Matheus Justin Hasda <matheusjustinhasda@gmail.com>
2025-06-28 01:43:59 +0600

    fix databas list display

--- core/admin.py
+++ core/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import Project
 
 # Register your models here.
-admin.site.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['subtitle']
+
+admin.site.register(Project, ProjectAdmin)
