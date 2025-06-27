713a8e3d4ef844ef080fee5652bb76b80e5fe71f
Matheus Justin Hasda <matheusjustinhasda@gmail.com>
2025-06-28 01:47:01 +0600

    add contact model and form

--- core/forms.py
+++ core/forms.py
@@ -1,7 +1,19 @@
 from django import forms
-from .models import Project
+from .models import Project, ContactMessage
 
 class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         fields = ['title', 'subtitle', 'description', 'preview_link', 'live_link']
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = ContactMessage
+        fields = ['name', 'email', 'subject', 'message']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your name'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter your email'}),
+            'subject': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter subject'}),
+            'message': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Type your message...', 'rows': 5}),
+        }
