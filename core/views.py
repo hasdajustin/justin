df6414333b52372f8c86a5b973e3bda79c836a41
Matheus Justin Hasda <matheusjustinhasda@gmail.com>
2025-06-28 00:59:57 +0600

    fix portfolio layout

--- core/views.py
+++ core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'core/index.html')
+
+def portfolio(request):
+    return render(request, 'core/portfolio.html')
