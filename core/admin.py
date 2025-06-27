577bb59da4743884c6eeaa3c4ef0a2272d362ad1
Matheus Justin Hasda <matheusjustinhasda@gmail.com>
2025-06-28 01:58:55 +0600

    fix contact page for sent data in database

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Project
+from .models import Project,ContactMessage
 
 # Register your models here.
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ['subtitle']
 
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['name', 'subject', 'submitted_at']
+
 admin.site.register(Project, ProjectAdmin)
+admin.site.register(ContactMessage, ContactAdmin)
--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Project
+from .forms import ContactForm
 
 # Create your views here.
 def home(request):
@@ -10,4 +12,12 @@ def portfolio(request):
     return render(request, 'core/portfolio.html', {'projects': projects})
 
 def contact(request):
-    return render(request, 'core/contact.html')
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Message sent successfully!")
+            return redirect('contact')
+    else:
+        form = ContactForm()
+    return render(request, 'core/contact.html', {'form': form})
